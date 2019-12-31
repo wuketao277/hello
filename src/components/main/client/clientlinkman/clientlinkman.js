be5d7308be9a3895f6304b2e3b070e9ddb8a773b
wuketao <wuketao@houbank.com>
2019-12-31 21:24:29 +0800

    add

---
+++ src/components/main/client/clientlinkman/clientlinkman.js
@@ -0,0 +1,170 @@
+export default {
+  data () {
+    return {
+      mode: 'add', // 默认操作模式为新建
+      form: {
+        id: null,
+        clientId: '',
+        chineseName: '',
+        englishName: '',
+        address: '',
+        phoneNo: '',
+        mobileNo: '',
+        email: ''
+      },
+      rules: {
+        chineseName: [
+          {
+            required: true,
+            message: '中文名必填',
+            trigger: 'blur'
+          },
+          {
+            max: 25,
+            message: '中文名长度不能大于25个字符',
+            trigger: 'blur'
+          }
+        ],
+        englishName: [{
+          max: 100,
+          message: '英文名长度不能大于100个字符',
+          trigger: 'blur'
+        }],
+        age: [{
+          max: 2,
+          message: '年龄不正确',
+          trigger: 'blur'
+        }],
+        phoneNo: [{
+          max: 20,
+          message: '电话号码长度不能大于20个字符',
+          trigger: 'blur'
+        }],
+        email: [{
+          max: 200,
+          message: '邮箱长度不能大于200个字符',
+          trigger: 'blur'
+        }],
+        companyName: [{
+          max: 200,
+          message: '公司名称长度不能大于200个字符',
+          trigger: 'blur'
+        }],
+        department: [{
+          max: 200,
+          message: '部门名称长度不能大于200个字符',
+          trigger: 'blur'
+        }],
+        title: [{
+          max: 200,
+          message: '职位名称长度不能大于200个字符',
+          trigger: 'blur'
+        }]
+      }
+    }
+  },
+  methods: {
+    // 保存 新评论
+    saveComment() {
+      if (this.newComment.content.length !== 0) {
+        // 组装数据
+        let comment = this.newComment
+        comment['candidateId'] = this.form.id
+        // 调用接口
+        commentApi.save(comment).then(res => {
+          if (res.status === 200) {
+            // 保存成功
+            this.$message({
+              message: '保存成功！',
+              type: 'success',
+              showClose: true
+            })
+            // 重新查询全部评论
+            this.queryComment()
+          } else {
+            this.$message({
+              message: '保存异常，请联系管理员！',
+              type: 'warning',
+              showClose: true
+            })
+          }
+        })
+      } else {
+        this.$message({
+          message: '请填写评论内容',
+          type: 'warning',
+          showClose: true
+        })
+      }
+    },
+    // 查询所有评论
+    queryComment() {
+      if (this.form.id.length !== 0) {
+        commentApi.findAllByCandidateId({
+          'candidateId': this.form.id
+        }).then(res => {
+          debugger
+          if (res.status === 200) {
+            this.comments = res.data
+          }
+        })
+      }
+    },
+    // 取消
+    cancel() {
+      if (typeof (this.$route.query.mode) !== 'undefined') {
+        this.mode = this.$route.query.mode
+        this.form = this.$route.query.candidate
+      } else {
+        this.form.id = ''
+        this.form.chineseName = ''
+        this.form.englishName = ''
+        this.form.age = 0
+        this.form.phoneNo = ''
+        this.form.email = ''
+        this.form.companyName = ''
+        this.form.department = ''
+        this.form.title = ''
+      }
+    },
+    // 保存新闻
+    save() {
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          // 如果校验通过就调用后端接口
+          candidateApi.save(this.form).then(
+            res => {
+              debugger
+              if (res.status === 200) {
+                // 将从服务端获取的id赋值给前端显示
+                this.form.id = res.data.id
+                this.$message({
+                  message: '保存成功！',
+                  type: 'success',
+                  showClose: true
+                })
+              }
+            })
+        } else {
+          // 如果检查不通过就给出提示
+          this.$message({
+            message: '有错误，请检查！',
+            type: 'warning',
+            showClose: true
+          })
+        }
+      })
+    }
+  },
+  computed: {},
+  created () {
+    // 通过入参获取当前操作模式
+    if (typeof (this.$route.query.mode) !== 'undefined') {
+      // 接收list传入的参数
+      this.mode = this.$route.query.mode
+      this.form = this.$route.query.candidate
+      // 查询comment
+      this.queryComment()
+    }
+  }
+}
---
+++ src/components/main/client/clientlinkman/clientlinkman.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-form
+      ref="form"
+      :model="form"
+      label-width="80px"
+      size="small"
+      style="margin-top:10px;text-align:left;"
+    >
+      <el-form-item label="id">
+        <span>{{form.id}}</span>
+      </el-form-item>
+      <el-form-item label="中文名">
+        <el-input v-model="form.chineseName"></el-input>
+      </el-form-item>
+      <el-form-item label="英文名">
+        <el-input v-model="form.englishName"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input v-model="form.mobileNo"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <el-input v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item label="地址">
+        <el-input v-model="form.address"></el-input>
+      </el-form-item>
+      <el-form-item label="座机">
+        <el-input v-model="form.phoneNo"></el-input>
+      </el-form-item>
+    </el-form>
+    <!--工具栏，只有模式为新增或修改时才显示-->
+    <div class="toolbar" v-show="(mode === 'add' || mode === 'modify')">
+      <el-button type="success" size="small" icon="el-icon-circle-check" @click="save">保存</el-button>
+      <el-button type="danger" size="small" icon="el-icon-delete" @click="cancel">取消</el-button>
+    </div>
+  </div>
+</template>
+<script src="./clientlinkman.js"></script>
