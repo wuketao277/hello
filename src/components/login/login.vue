93b6314207f9452cd0aae13579dd53cd3bd72e9a
wuketao <wuketao@houbank.com>
2021-02-11 14:51:17 +0800

    修改

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -4,19 +4,18 @@
       <el-form
         ref="form"
         :model="loginUser"
-        label-width="55px"
-        style="width:420px;height:280px;padding-top:10px;padding-left:10px;padding-right:10px;position:absolute;left:50%;top:40%;margin-left:-210px;margin-top:-120px;border:2px solid #609EFF;border-radius:5px;"
+        label-width="0px"
+        style="background-color:#fff;width:420px;height:280px;padding-top:10px;padding-left:10px;padding-right:10px;position:absolute;left:50%;top:40%;margin-left:-210px;margin-top:-120px;border:2px solid #609EFF;border-radius:5px;"
       >
         <h1>欢迎使用Hello人才系统</h1>
-        <el-form-item label="用户名">
-          <el-input v-model="loginUser.loginName"></el-input>
+        <el-form-item>
+          <el-input v-model="loginUser.loginName" placeholder="用户名"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="loginUser.password"></el-input>
+        <el-form-item>
+          <el-input v-model="loginUser.password" placeholder="密码"></el-input>
         </el-form-item>
         <div>
-          <el-button type="primary" size="small" @click="login">登录</el-button>
-          <el-button type="primary" size="small" @click="logincancel">取消</el-button>
+          <el-button type="primary" size="big" @click="login">登录</el-button>
         </div>
       </el-form>
     </div>
