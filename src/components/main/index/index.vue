b740399ef582b6c97301d4dcca57e5976650ae23
wuketao277 <howard.wu@helloapplicant.com>
2022-05-05 19:50:20 +0800

    优化

--- src/components/main/index/index.vue
+++ src/components/main/index/index.vue
@@ -6,9 +6,7 @@
       <div style="float:left;height:100%;text-align:left;padding-left:20px;">
         <span>Hello System</span>
       </div>
-      <div
-        style="float:right;width:50%;text-align:right;padding-top:4px;padding-right:20px;"
-      >
+      <div style="float:right;width:50%;text-align:right;padding-top:4px;padding-right:20px;">
         <span>Hi {{username}}</span>
         <el-button
           type="text"
@@ -37,38 +35,51 @@
               <span slot="title">主页</span>
             </template>
           </el-menu-item>
-          <el-submenu index="1" v-if="showControl('/report')">
+          <el-menu-item index="/report/general" v-if="showControl('/report')">
             <template slot="title">
               <i class="el-icon-s-data"></i>
               <span slot="title">报表</span>
             </template>
-            <el-menu-item index="/report/general">总报表</el-menu-item>
-          </el-submenu>
-          <el-submenu index="2">
+          </el-menu-item>
+          <el-menu-item index="/client/clientlist">
             <template slot="title">
               <i class="el-icon-user"></i>
-              <span slot="title">客户</span>
+              <span slot="title">客户列表</span>
             </template>
-            <el-menu-item index="/client/clientlist">客户列表</el-menu-item>
-            <el-menu-item index="/client/client">新增客户</el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
+          </el-menu-item>
+          <el-menu-item index="/case/caselist">
             <template slot="title">
               <i class="el-icon-star-off"></i>
-              <span slot="title">职位</span>
+              <span slot="title">职位列表</span>
             </template>
-            <el-menu-item index="/case/caselist">职位列表</el-menu-item>
-            <el-menu-item index="/case/case">新增职位</el-menu-item>
-            <el-menu-item index="/case/successfulPermList">成功职位列表</el-menu-item>
-            <el-menu-item index="/case/successfulPerm">成功职位</el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
+          </el-menu-item>
+          <el-menu-item index="/case/successfulPermList">
+            <template slot="title">
+              <i class="el-icon-star-on"></i>
+              <span slot="title">成功列表</span>
+            </template>
+          </el-menu-item>
+          <el-menu-item index="/candidate/candidatelist">
             <template slot="title">
               <i class="el-icon-user-solid"></i>
               <span slot="title">候选人</span>
             </template>
-            <el-menu-item index="/candidate/candidatelist">候选人列表</el-menu-item>
-            <el-menu-item index="/candidate/candidate">新增候选人</el-menu-item>
+          </el-menu-item>
+          <el-menu-item index="/file/filelist">
+            <template slot="title">
+              <i class="el-icon-document"></i>
+              <span slot="title">文件管理</span>
+            </template>
+          </el-menu-item>
+          <el-submenu index="7">
+            <template slot="title">
+              <i class="el-icon-money"></i>
+              <span slot="title">工资</span>
+            </template>
+            <el-menu-item index="/salary/salaryList">工资列表</el-menu-item>
+            <el-menu-item index="/salary/salarySpecialItemList">工资特殊项列表</el-menu-item>
+            <el-menu-item index="/salary/reimbursementSummaryList">报销发放列表</el-menu-item>
+            <el-menu-item index="/salary/reimbursementItemList">报销项详情列表</el-menu-item>
           </el-submenu>
           <el-submenu index="5">
             <template slot="title">
@@ -78,28 +89,6 @@
             <el-menu-item index="/mynews/mynewslist">我的新闻</el-menu-item>
             <el-menu-item index="/mytask/mytasklist">我的任务</el-menu-item>
             <!-- <el-menu-item index="/my/myplan/planlist">我的计划</el-menu-item> -->
-          </el-submenu>
-          <el-submenu index="6">
-            <template slot="title">
-              <i class="el-icon-document"></i>
-              <span slot="title">文件管理</span>
-            </template>
-            <el-menu-item index="/file/filelist">文件管理</el-menu-item>
-          </el-submenu>
-          <el-submenu index="7">
-            <template slot="title">
-              <i class="el-icon-money"></i>
-              <span slot="title">工资</span>
-            </template>
-            <el-menu-item index="/salary/salaryList">工资列表</el-menu-item>
-            <el-menu-item index="/salary/salarySpecialItemList">工资特殊项列表</el-menu-item>
-            <el-menu-item
-              index="/salary/salarySpecialItem"
-              v-show="showControl('/salary/salarySpecialItem')"
-            >工资特殊项</el-menu-item>
-            <el-menu-item index="/salary/reimbursementSummaryList">报销发放列表</el-menu-item>
-            <el-menu-item index="/salary/reimbursementItemList">报销项详情列表</el-menu-item>
-            <el-menu-item index="/salary/reimbursementItem">报销项</el-menu-item>
           </el-submenu>
           <el-submenu index="8">
             <template slot="title">
