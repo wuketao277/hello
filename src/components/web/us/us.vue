69b47e6850163a25fbee4336eb1c61bca390a377
wuketao277 <howard.wu@helloapplicant.com>
2023-10-31 08:53:15 +0800

    优化

--- src/components/web/us/us.vue
+++ src/components/web/us/us.vue
@@ -1,6 +1,51 @@
 <template>
-  <div style="padding-top:100px;padding-left:100px;padding-right:100px;">
-    <h1>关于我们</h1>
+  <div style="padding-top:100px;padding-left:100px;padding-right:100px;text-align:left;">
+    <span style="font-size:20px;">
+      上海海罗人才服务有限公司成立于2019年08月15日，为海罗人才集团（Hello Applicant Talent Solution Group）的第一家注册公司，集团总部位于上海，在北京，南京，武汉，沈阳分别设有独立的分支或办公机构。
+    </span>
+    <br />
+    <span>
+      公司名称：上海海罗人才服务有限公司<br />
+      外文名：Shanghai Hello Applicant Talent Service Co.,Ltd.<br />
+      公司类型：人力资源一站式服务提供商<br />
+      公司理念：All Embrace<br />
+    </span>
+    <br /><br />
+    <span style="font-size:20px;">
+      上海海罗人才科技有限公司成立于2022年09月07日，为海罗人才集团（Hello Applicant Talent Solution Group）的第二家注册公司，集团总部位于上海，在北京、南京、武汉、沈阳分别设有独立的分支或办公机构。
+    </span>
+    <br />
+    <span>
+      公司名称：上海海罗人才科技有限公司<br />
+      外文名：Shanghai Hello Applicant Talent Technology Co.,Ltd.<br />
+      公司类型：人力资源一站式服务提供商<br />
+      公司理念：All Embrace<br />
+      总部地点：上海市浦东新区<br />
+    </span>
+    <br /><br />
+    <span style="font-size:20px;">
+      沈阳海罗人才服务有限公司成立于2022年09月09日，为海罗人才集团（Hello Applicant Talent Solution Group）的第三家注册公司，集团总部位于上海，在北京、南京、武汉、沈阳分别设有独立的分支或办公机构。
+    </span>
+    <br />
+    <span>
+      公司名称：沈阳海罗人才服务有限公司<br />
+      外文名：Shenyang Hello Applicant Talent Service Co.,Ltd.<br />
+      公司类型：人力资源一站式服务提供商<br />
+      公司理念：All Embrace<br />
+      沈阳总部地点：辽宁省沈阳市皇姑区<br />
+    </span>
+    <br /><br />
+    <span style="font-size:20px;">
+      武汉海罗河山人才服务有限公司成立于2023年06月25日，为海罗人才集团（Hello Applicant Talent Solution Group）的第五家注册公司，集团总部位于上海，在北京、南京、武汉、沈阳分别设有独立的分支或办公机构。
+    </span>
+    <br />
+    <span>
+      公司名称：武汉海罗河山人才服务有限公司<br />
+      外文名：Wuhan Hello Applicant Talent Service Co.,Ltd.<br />
+      公司类型：人力资源一站式服务提供商<br />
+      公司理念：All Embrace<br />
+      武汉总部地点：湖北省武汉市武昌区<br />
+    </span>
   </div>
 </template>
 <style>
